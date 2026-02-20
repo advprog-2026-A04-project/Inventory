96c90c09d9b972988ddc57d0b79e3bfe7554f99b
FakhriHusainiRomza23 <fakhri.husaini41@ui.ac.id>
2026-02-20 16:20:45 +0700

    add concurrency test

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
